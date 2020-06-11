11a6183fc1d6ed451e1d19b836f89006a5feb572
chenhongde <634105393@qq.com>
2020-06-11 15:05:16 +0800

    第一次提交

---
+++ mode1/src/main/java/com/mode/mode1/Mode1Application.java
@@ -0,0 +1,13 @@
+package com.mode.mode1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mode1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mode1Application.class, args);
+    }
+
+}
