e873ca8006229d63e762244d1efb5eea275c58a9
chenhongde <chenhongde>
2021-06-17 16:00:00 +0800

    feat(单例) 单例模式
    新建项目

---
+++ mode1/src/test/java/com/mode/mode1/Mode1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mode.mode1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mode1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
